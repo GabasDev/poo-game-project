2d3cbd28c1021df80d65ef6ac1b06359835a76bb
rallviz <ralvizpro@gmail.com>
2024-07-30 20:18:33 -0300

    acrescentando classe medidor de força

---
+++ src/medidor_de_forca.py
@@ -0,0 +1,27 @@
+import pygame
+
+class MedidorDeForca:
+    def init(self):
+        self.arremesso_ativo = False
+        self.tempo_arremesso_inicio = None
+
+    def iniciar_arremesso(self):
+        self.arremesso_ativo = True
+        self.tempo_arremesso_inicio = pygame.time.get_ticks()
+
+    def finalizar_arremesso(self):
+        self.arremesso_ativo = False
+
+    def calcular_forca(self):
+        if self.arremesso_ativo:
+            tempo_arremesso = (pygame.time.get_ticks() - self.tempo_arremesso_inicio) / 1000.0
+            return min(tempo_arremesso * 20, 100)
+        return 0
+
+    def desenhar(self, tela):
+        forca = self.calcular_forca()
+        largura_maxima = 200
+        altura = 20
+        largura = (forca / 100) * largura_maxima
+        pygame.draw.rect(tela, (255, 255, 255), (300, 500, largura_maxima, altura), 2)
+        pygame.draw.rect(tela, (255, 255, 255), (300, 500, largura, altura))
