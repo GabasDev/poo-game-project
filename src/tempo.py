32a0d6e7315168c9529bcb02de56e153afb11f7d
JCarlosSousa <carlossousa.gm@gmail.com>
2024-07-30 15:23:55 -0300

    Adicionado a Classe Tempo ao código.

---
+++ src/tempo.py
@@ -0,0 +1,18 @@
+class Tempo:
+    def init(self, tempoMaximo):
+        self.tempoMaximo = tempoMaximo
+        self.tempo_inicial = pygame.time.get_ticks()
+        self.tempo_restante = self.tempoMaximo * 1000
+
+    def atualizar_tempo_restante(self):
+        "Atualiza o tempo restante do jogo."
+        tempo_atual = pygame.time.get_ticks()
+        self.tempo_restante = max(0, self.tempoMaximo * 1000 - (tempo_atual - self.tempo_inicial))
+
+    def get_tempo_restante(self):
+        return self.tempo_restante
+
+    def formatar_tempo(self):
+        minutos = self.tempo_restante // 60000
+        segundos = (self.tempo_restante % 60000) // 1000
+        return f"Tempo: {minutos:02}:{segundos:02}"
