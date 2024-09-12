8310c71ff78e94a7d7408239476a6a36d2f7cab1
Renato Alves <110750708+rallviz@users.noreply.github.com>
2024-09-11 21:38:39 -0300

    new class bulba

---
+++ src/Pok_bulbassaur.py
@@ -0,0 +1,31 @@
+import pygame
+import random
+import math
+from .pokemon import Pokemon
+
+class Bulbasaur(Pokemon):
+    def _init_(self, lista_pokemons):
+        super()._init_(lista_pokemons, "static/imagens/Bulbassauro.png")
+        self._posicionar()
+        self.velocidade = random.randint(1, 3)
+        "Inicia com um ângulo aleatório"
+        self.angulo = random.uniform(0, 2 * math.pi)  
+    def mover(self):
+        "Movimento circular"
+        centro_x, centro_y = 400, 300
+        raio = 100
+        "Atualiza o ângulo com base na velocidade"
+        self.angulo += 0.01 * self.velocidade  
+
+        self.rect.x = centro_x + raio * math.sin(self.angulo) - self.rect.width / 2
+        self.rect.y = centro_y + raio * math.cos(self.angulo) - self.rect.height / 2
+
+        "Garante que o Pokémon permaneça dentro dos limites da tela"
+        if self.rect.right < 0:
+            self.rect.left = 800
+        elif self.rect.left > 800:
+            self.rect.right = 0
+        if self.rect.bottom < 0:
+            self.rect.top = 600
+        elif self.rect.top > 600:
+            self.rect.bottom = 0
